1dd01103420becf7acb63a86bdaa91493bb35b05
Jonathan Wells <jnw72@cornell.edu>
2020-11-23 18:30:10 -0500

    Minor changes in analysis scripts.

--- scripts/gene-annotation/curate_finz.py
+++ scripts/gene-annotation/curate_finz.py
@@ -20,25 +20,27 @@ with open('../../data/gffs/Danio_rerio_finz.final.gff', 'w') as outfile:
             if line[2] == 'gene':
                 gid = re.match('ID=(\w+)$', line[-1]).group(1)
                 id_replacement = gene_dict.get(gid, gid)
-                line[-1] = f'ID=gene:{id_replacement}'
+                line[-1] = f'ID={id_replacement}'
             elif line[2] == 'transcript':
                 gid = re.match('ID=(\w+\.t1);Parent=(\w+)$', line[-1])
                 id_replacement = transcript_dict.get(gid.group(1), gid.group(1)).strip('transcript:')
                 parent_replacement = gene_dict.get(gid.group(2), gid.group(2))
-                line[-1] = f'ID=transcript:{id_replacement};Parent=gene:{parent_replacement}'
+                line[-1] = f'ID={id_replacement};Parent={parent_replacement}'
                 line[2] = 'mRNA'
             elif line[2] in ['start_codon', 'stop_codon']:
                 gid = re.match('Parent=(\w+\.t1)$', line[-1]).group(1)
                 parent_replacement = transcript_dict.get(gid, gid).strip('transcript:')
-                line[-1] = f'Parent=transcript:{parent_replacement}'
+                line[-1] = f'Parent={parent_replacement}'
             elif line[2] == 'CDS':
                 gid = re.match('ID=(\w+\.t1)\.cds;Parent=(\w+\.t1)$', line[-1])
                 id_replacement = transcript_dict.get(gid.group(1), gid.group(1)).strip('transcript:')
                 parent_replacement = transcript_dict.get(gid.group(2), gid.group(2)).strip('transcript:')
                 line[2] = 'exon'
-                line[-1] = f'ID=transcript:{id_replacement}.exon;Parent=transcript:{parent_replacement}'
+                line[-1] = f'ID={id_replacement}.exon;Parent={parent_replacement}'
                 outfile.write('\t'.join(line) + '\n')
                 line[2] = 'CDS'
-                line[-1] = f'ID=transcript:{id_replacement}.cds;Parent=transcript:{parent_replacement}'
+                line[-1] = f'ID={id_replacement}.cds;Parent={parent_replacement}'
             outfile.write('\t'.join(line) + '\n')
 
+## Best to remove all the "transcript:" and "gene:" after with sed
+# sed 's/transcript://g'  Danio_rerio.GRCz11.101.curated_finz.gff3 | sed 's/gene://g'
--- scripts/gene-annotation/merge_curated_finz.sh
+++ scripts/gene-annotation/merge_curated_finz.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-# Purpose of this script is to combine Ensembl gff files with curated denovo
-# finz-znf genes.
+# Purpose of this script is to combine Ensembl annotations with curated denovo
+# finz-znfs and repeatmasker output. Converts to gtf file for downstream STAR
 
 ./rename_ensembl_chroms.py ../../data/gffs/Danio_rerio.GRCz11.101.gff3 subtracted.base.gff3
-awk '{ OFS="\t" }{ print $2, $3, $4, $5, $6, $7}' ../../data/gffs/ensembl_finz_znf.gff > subtracted.txt
-rg -v -f subtracted.txt subtracted.base.gff3 > subtracted.gff3
-cat subtracted.gff3 ../../data/gffs/Danio_rerio_finz.final.gff > subtracted.merged.gff3
-bedtools sort -header -i subtracted.merged.gff3 > ../../data/gffs/Danio_rerio.GRCz11.101.curated_finz.gff3
-rm subtracted*
+gffread -T subtracted.base.gff3 -o subtracted.base.gtf
+gffread -T ../../data/gffs/ensembl_finz_znf.gff | awk '{ OFS="\t" }{ print $2, $3, $4, $5, $6, $7}' > subtracted.txt
+rg -v -f subtracted.txt subtracted.base.gtf > subtracted.gtf
+gffread -T ../../data/gffs/Danio_rerio_finz.final.gff > ../../data/gffs/Danio_rerio_finz.final.gtf 
+cat subtracted.gtf ../../data/gffs/Danio_rerio_finz.final.gtf > ../../data/gffs/Danio_rerio.GRCz11.101.curated_finz.gtf
+cat ../../data/gffs/Danio_rerio.GRCz11.101.curated_finz.gtf ../../data/gffs/danRer11.nonalt.tetranscripts.gtf > ../../data/expression/danRer11.nonalt.genes_tes.gtf
+
+sed 's/transcript://g' ../../data/gffs/Danio_rerio.GRCz11.101.curated_finz.gtf | sed 's/gene://g' > tmp
+mv tmp ../../data/gffs/Danio_rerio.GRCz11.101.curated_finz.gtf
+sed 's/transcript://g' ../../data/gffs/danRer11.nonalt.tetranscripts.gtf | sed 's/gene://g' > tmp
+mv tmp ../../data/gffs/danRer11.nonalt.tetranscripts.gtf
+sed 's/transcript://g' ../../data/expression/danRer11.nonalt.genes_tes.gtf | sed 's/gene://g' > tmp
+mv tmp ../../data/expression/danRer11.nonalt.genes_tes.gtf
+
+rm subtracted* ../../data/gffs/Danio_rerio_finz.final.gtf
 
