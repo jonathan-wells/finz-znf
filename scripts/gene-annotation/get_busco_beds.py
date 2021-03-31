2dad3ad0d5e25be54515e47f314e65f5a1f0640d
Jonathan Wells <jnw72@cornell.edu>
2021-03-31 16:04:07 -0400

    create busco bed files

--- scripts/gene-annotation/get_busco_beds.py
+++ scripts/gene-annotation/get_busco_beds.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
 import os
+from Bio import SeqIO
+import re
 
 scbuscos = {}
 for species in ['Danio_albolineatus',
                 'Danio_choprai',
                 'Danio_jaintianensis',
                 'Danio_tinwini']:
-    scbuscos[species] = os.listdir(f'../../data/busco-out/{species}/run_actinopterygii_odb10/busco_sequences/single_copy_busco_sequences')
+    scbuscos[species] = set(os.listdir(f'../../data/busco-out/{species}/run_actinopterygii_odb10/busco_sequences/single_copy_busco_sequences'))
 
-print(scbuscos)
+scbuscoset = set.intersection(*scbuscos.values())
+
+for species in ['Danio_albolineatus',
+                'Danio_choprai',
+                'Danio_jaintianensis',
+                'Danio_tinwini']:
+    with open(f'../../data/danio-reads/{species}_busco.bed', 'w') as outfile:
+        for bid in list(scbuscoset)[:20]:
+            record = list(SeqIO.parse(f'../../data/busco-out/{species}/run_actinopterygii_odb10/busco_sequences/single_copy_busco_sequences/{bid}',
+                'fasta'))[0]
+            chrom, start, stop = re.split(':|-', record.id)
+            if int(start) > int(stop):
+                start, stop = stop, start
+            line = [chrom, start, stop, species + '_' + bid.strip('.faa')]
+            outfile.write('\t'.join(line) + '\n')
+
+
