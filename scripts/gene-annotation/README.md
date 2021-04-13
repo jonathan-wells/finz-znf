0dfc96ca3aa093734b64ae59087de17f83bd1816
Jonathan Wells <jnw72@cornell.edu>
2021-04-13 19:26:48 -0400

    Add readme and finish busco exon script

---
+++ scripts/gene-annotation/README.md
@@ -0,0 +1,20 @@
+# Gene Annotation Scripts
+
+Lots going on here, but there are two main groups of scripts:
+
+## Annotation of FiNZ-ZNF genes
+1. `generate_profiles.sh`
+2. `predict_finz.sh`
+3. ``
+
+## Quality control:
+### Read coverage over predicted genes.
+1. `get_busco_beds.py`
+2. `extract_flanked_genes.sh`
+3. `map_danio_reads.sh`
+4. `blast_busco_exons.sh`
+
+### Comparison with 
+Various accessory scripts:
+
+
--- scripts/gene-annotation/blast_busco_exons.sh
+++ scripts/gene-annotation/blast_busco_exons.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
-for busco in 13830at7898; do
-    tblastn \
-        -query "../../data/danio-reads/${busco}.faa" \
-        -subject ../../data/danio-reads/Danio_choprai_flanked_genes.fa \
-        -outfmt "6 sseqid sstart send " \
-        -evalue 1e-10 \
-        -out test.out
-    awk -v buscovar="${busco}" '{ OFS="\t" } {
-        if ($2 > $3) 
-            print $1, $3, $2, buscovar, ".",".", "BUSCO", "CDS", 0
-        else
-            print $1, $2, $3, buscovar, ".",".", "BUSCO", "CDS", 0
-    }' test.out > test2.out 
-done    
+species=(
+    Danio_choprai
+    Danio_jaintianensis
+    Danio_albolineatus
+    Danio_tinwini
+    )
+
+for sp in ${species[@]}; do
+    echo $sp
+    rg "\d+at\d+" ../../data/danio-reads/${sp}_flanked_genes.fa |
+        sed "s/>${sp}_//" > tmp_buscos.txt
+
+    while read -r busco; do
+        tblastn \
+            -query "../../data/busco-out/${sp}/run_actinopterygii_odb10/busco_sequences/single_copy_busco_sequences/${busco}.faa" \
+            -subject ../../data/danio-reads/${sp}_flanked_genes.fa \
+            -outfmt "6 sseqid sstart send " \
+            -evalue 1e-10 \
+            -out tmp.out
+        awk -v buscovar="${busco}" '{ OFS="\t" } {
+            if ($2 > $3) 
+                print $1, $3, $2, buscovar, ".",".", "BUSCO", "CDS", 0
+            else
+                print $1, $2, $3, buscovar, ".",".", "BUSCO", "CDS", 0
+        }' tmp.out >> "${sp}.out" 
+    done < tmp_buscos.txt 
+done 
+rm tmp_buscos.txt tmp.out
+
