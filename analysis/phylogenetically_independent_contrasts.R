ffb4a9892674e90395629fde19060d453375c6e5
Jonathan Wells <jnw72@cornell.edu>
2021-06-10 11:40:12 -0400

    Just continue analysis, nothing noteworthy

--- analysis/phylogenetically_independent_contrasts.R
+++ analysis/phylogenetically_independent_contrasts.R
@@ -8,10 +8,10 @@ library(ggrepel)
 library(stringr)
 
 
-cypriniformes.tree <- read.tree(file="/Users/jonwells/Projects/feschottelab/finz-znf/data/species-phylogeny/busco_supermatrix_partitions.nex.treefile")
+cypriniformes.tree <- read.tree(file="/Users/jonwells/Projects/feschottelab/finz-znf/data/species-phylogeny/iqtree-data/busco_supermatrix_partitions.nex.timetree.nwk")
 cypriniformes.tree <- midpoint.root(cypriniformes.tree)
 
-keepspecies <- read.csv('/Users/jonwells/Projects/feschottelab/finz-znf/data/n50_gt5e05.txt', header=FALSE)
+keepspecies <- read.csv('/Users/jonwells/Projects/feschottelab/finz-znf/data/hiqual_species.txt', header=FALSE)
 currspecies = cypriniformes.tree$tip.label
 for (tip in currspecies) {
   if (!(tip %in% keepspecies$V1)) {
@@ -19,7 +19,6 @@ for (tip in currspecies) {
     }
 }
 
-# cypriniformes.tree <- drop.tip(cypriniformes.tree, cypriniformes.tree$tip.label[startsWith(cypriniformes.tree$tip.label, "Triplophysa")])
 plot(cypriniformes.tree)
 
 counts.df <- read.csv("/Users/jonwells/Projects/feschottelab/finz-znf/data/finz_te_counts.txt", sep='\t')
@@ -29,12 +28,12 @@ counts.df <- counts.df[order(counts.df$species),]
 
 
 fit <- lm(counts.df$finz_znf~counts.df$interspersed)
-fig1 <- ggplot(counts.df, aes(x=interspersed, y=finz_exons, label=species)) + 
-  geom_point() +ggop
+fig1 <- ggplot(counts.df, aes(x=interspersed, y=finz_znf, label=species)) + 
+  geom_point() +
   geom_text_repel() +
   geom_smooth(method='lm', formula=y~x)
 fig1
-cor.test(counts.df$finz_exons, counts.df$interspersed, method="s")
+cor.test(counts.df$finz_znf, counts.df$interspersed, method="s")
 
 
 pic.df <- data.frame(pic.finz_exons = pic(counts.df$finz_exons, cypriniformes.tree)) %>%
@@ -44,13 +43,20 @@ pic.df <- data.frame(pic.finz_exons = pic(counts.df$finz_exons, cypriniformes.tr
          pic.LTR = pic(counts.df$LTR, cypriniformes.tree),
          pic.LINE = pic(counts.df$LINE, cypriniformes.tree),
          pic.SINE = pic(counts.df$SINE, cypriniformes.tree))
+write.table(x=pic.df, 
+            file="/Users/jonwells/Projects/feschottelab/finz-znf/data/finz_pics.txt", 
+            row.names=F,
+            sep="\t", 
+            quote=F)
 
-fig2 <- ggplot(pic.df, aes(x=pic.interspersed, y=pic.finz_exons)) +
+fig2 <- ggplot(pic.df, aes(x=pic.interspersed, y=pic.finz_znf)) +
   geom_point() 
 fig2
+cor.test(pic.df$pic.finz_znf, pic.df$pic.interspersed, method="s")
 
 fit <- lm(formula = pic.df$pic.finz_exons~pic.df$pic.interspersed)
 summary(fit)
 cor.test(pic.df$pic.finz_exons, pic.df$pic.interspersed, method="s")
 
 
+  
