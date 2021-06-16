86b597fc7e1d9336bd95690faf38a50ad12d2203
Jonathan Wells <jnw72@cornell.edu>
2021-06-16 09:53:44 -0400

    Tweaks to finz prediction pipeline

--- scripts/gene-annotation/predict_finz.sh
+++ scripts/gene-annotation/predict_finz.sh
@@ -8,83 +8,87 @@ genomedir='/Users/jonwells/Genomes/Cypriniformes/ncbi-genomes-2020-07-09'
 
 declare -A genomes
 
-while read line; do
-    key=`awk '{ print $1 }' <<< $line`
-    data=`awk '{ print $2 }' <<< $line`
-    genomes[$key]="$data"
-done < ../../data/species_genomes.txt
-
-#for species in ${!genomes[@]}; do
-#    genomefile=${genomes[$species]}
-#    echo $species
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    # 1. Extract genome contig/scaffold/chromosome lengths
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    bioawk -c fastx '{ 
-#        OFS="\t" 
-#    } { 
-#        print $name, length($seq) 
-#    }' "${genomedir}/${genomefile}" > "../../data/beds/${species}.genome" 
+# while read line; do
+#     key=`awk '{ print $1 }' <<< $line`
+#     data=`awk '{ print $2 }' <<< $line`
+#     genomes[$key]="$data"
+# done < ../../data/species_genomes.txt
 
 
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    # 2. tblastn to get coordinates of protein domains
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    tblastn \
-#        -query ../../data/phmms/finz_seed.consensus.fa \
-#        -subject "${genomedir}/${genomefile}" \
-#        -evalue 1e-03 \
-#        -num_threads 8 \
-#        -outfmt 6 \
-#        -out "../../data/blast-out/${species}_finz_locs.out"
+genomes=(
+    [Danio_rerio]="GCF_000002035.6_GRCz11_genomic.nonalt.remasked.fna"
+)
+for species in ${!genomes[@]}; do
+    genomefile=${genomes[$species]}
+    echo $species
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # 1. Extract genome contig/scaffold/chromosome lengths
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    bioawk -c fastx '{ 
+        OFS="\t" 
+    } { 
+        print $name, length($seq) 
+    }' "${genomedir}/${genomefile}" > "../../data/beds/${species}.genome" 
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # 2. tblastn to get coordinates of protein domains
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    tblastn \
+        -query ../../data/phmms/finz_seed.consensus.fa \
+        -subject "${genomedir}/${genomefile}" \
+        -evalue 1e-03 \
+        -outfmt 6 \
+        -out "../../data/blast-out/${species}_finz_locs.out"
     
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    # 3. Extract sequence from blocks >= 100kb around FINZ hits
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    awk '{ 
-#        OFS="\t"
-#        } {
-#        if ( $9 < $10 )
-#            print $2, $9, $10 
-#        else
-#            print $2, $10, $9
-#        }' "../../data/blast-out/${species}_finz_locs.out" |
-#            bedtools sort > "../../data/beds/${species}_finz.bed"
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # 3. Extract sequence from blocks >= 100kb around FINZ hits
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    awk '{ 
+        OFS="\t"
+        } {
+        if ( $9 < $10 )
+            print $2, $9, $10 
+        else
+            print $2, $10, $9
+        }' "../../data/blast-out/${species}_finz_locs.out" |
+            bedtools sort > "../../data/beds/${species}_finz.bed"
 
-#    # Extract blocks
-#    bedtools slop \
-#        -b 50000 \
-#        -i "../../data/beds/${species}_finz.bed" \
-#        -g "../../data/beds/${species}.genome" |
-#        bedtools merge > "../../data/beds/${species}_finz_blocks.bed"
+    # Extract blocks
+    bedtools slop \
+        -b 50000 \
+        -i "../../data/beds/${species}_finz.bed" \
+        -g "../../data/beds/${species}.genome" |
+        bedtools merge > "../../data/beds/${species}_finz_blocks.bed"
 
-#    # Extract sequence
-#    bedtools getfasta \
-#        -fi "${genomedir}/${genomefile}" \
-#        -bed "../../data/beds/${species}_finz_blocks.bed" > "../../data/seqs/${species}_finz_blocks.fa"
+    # Extract sequence
+    bedtools getfasta \
+        -fi "${genomedir}/${genomefile}" \
+        -bed "../../data/beds/${species}_finz_blocks.bed" > "../../data/seqs/${species}_finz_blocks.fa"
 
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    # 4. Augustus-ppx to annotate predicted FINZ-znfs
-#    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#    /usr/local/Cellar/augustus/3.3.3_1/bin/augustus \
-#        --genemodel=complete \
-#        --optCfgFile=/usr/local/Cellar/augustus/3.3.3_1/config/ppx.cfg \
-#        --proteinprofile=../../data/phmms/drerio_finz_expressed.prfl \
-#        --species=zebrafish \
-#        "../../data/seqs/${species}_finz_blocks.fa" > "../../data/gffs/${species}_augustus_finz.gff"
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # 4. Augustus-ppx to annotate predicted FINZ-znfs
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /usr/local/Cellar/augustus/3.3.3_1/bin/augustus \
+        --genemodel=complete \
+        --optCfgFile=/usr/local/Cellar/augustus/3.3.3_1/config/ppx.cfg \
+        --proteinprofile=../../data/phmms/drerio_finz_expressed.prfl \
+        --species=zebrafish \
+        --softmasking=1 \
+        "../../data/seqs/${species}_finz_blocks.fa" > "../../data/gffs/${species}_augustus_finz.gff"
 
-#    # Extract all protein sequences
-#    /usr/local/Cellar/augustus/3.3.3_1/scripts/getAnnoFasta.pl \
-#        "../../data/gffs/${species}_augustus_finz.gff" \
-#        --seqfile="../../data/seqs/${species}_finz_blocks.fa"
+    # Extract all protein sequences
+    /usr/local/Cellar/augustus/3.3.3_1/scripts/getAnnoFasta.pl \
+        "../../data/gffs/${species}_augustus_finz.gff" \
+        --seqfile="../../data/seqs/${species}_finz_blocks.fa"
     
-#       # Rename seqs by prepending species name
-#    for suffix in codingseq cdsexons aa; do
-#        mv "../../data/gffs/${species}_augustus_finz.${suffix}" "../../data/seqs/${species}_augustus_finz.${suffix}.fa"
-#        sed "s/>/>${species}_/g" "../../data/seqs/${species}_augustus_finz.${suffix}.fa" > tmp.fa
-#        mv tmp.fa "../../data/seqs/${species}_augustus_finz.${suffix}.fa"
-#    done
-#done
+    # Rename seqs by prepending species name
+    for suffix in codingseq cdsexons aa; do
+        mv "../../data/gffs/${species}_augustus_finz.${suffix}" "../../data/seqs/${species}_augustus_finz.${suffix}.fa"
+        sed "s/>/>${species}_/g" "../../data/seqs/${species}_augustus_finz.${suffix}.fa" > tmp.fa
+        mv tmp.fa "../../data/seqs/${species}_augustus_finz.${suffix}.fa"
+    done
+done
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## 5. HMMER to extract only predicted FINZ-ZNFs
