f2a6357c21481f7b3933c01bc01b89c7b3a06ddc
Jonathan Wells <jnw72@cornell.edu>
2021-06-17 14:38:14 -0400

    Change output directory location and reduce number of CPUs requested.

--- scripts/gene-annotation/extract_mrnas.sh
+++ scripts/gene-annotation/extract_mrnas.sh
@@ -9,29 +9,29 @@ for stage in "Dome" "128-cell" "1k-cell" "50pc-epiboly" "Shield" "75pc-epiboly";
         if ($NF == "RNASeq" && $6 == stage ) 
             print "../../data/expression/STARaligned/"$3".bam";
     }' ../../data/expression/White2017/elife-30860-supp1-v1.tsv > "${stage}.names"
-
+    
     # Merge and sort relevant bamfiles
     samtools merge \
-        -b "${stage}.names" "${stage}.bam" \
-        -h \
+        -f \
         -c \
         -p \
-        -@ 25
+        -b "${stage}.names" \
+        -@ 30 \
+        "${stage}.bam"
     samtools sort \
         -o "${stage}.sorted.bam" \
         -O BAM \
         --reference ../../data/expression/STARgenome/GCF_000002035.6_GRCz11_genomic.nonalt.fna \
-        -@ 25 \
+        -@ 30 \
         "${stage}.bam"
     rm "${stage}.bam" "${stage}.names"
-
 
     # Run Trinity in genome-guided mode to extract transcripts
     echo running trinity for stage $stage
     /workdir/jnw72/Software/trinityrnaseq-v2.12.0/Trinity \
-        --genome_guided_bam "${stage}.sorted.bam"  \
-        --max_memory 500G \
+        --genome_guided_bam "${stage}.sorted.bam" \
+        --max_memory 250G \
         --genome_guided_max_intron 10000 \
-        --CPU 25 \
-        --output "../../data/expression/trinity-transcripts/trinity-${stage}"
+        --CPU 17
+    mv trinity_out_dir "../../../data/trinity-out/trinity-${stage}"
 done
